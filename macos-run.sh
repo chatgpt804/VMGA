b28448504fd5591bbca1c4648a7f635de7a755e2
chatgpt804 <h59.6.07.5.04@gmail.com>
2025-08-19 22:07:05 +0545

    Update macos-run.sh

--- macos-run.sh
+++ macos-run.sh
@@ -1,34 +1,37 @@
-#macos-run.sh MAC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN MAC_REALNAME
+#!/bin/bash
+# macos-run.sh MAC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN MAC_REALNAME
 
-#disable spotlight indexing
+# disable spotlight indexing
 sudo mdutil -i off -a
 
-#Create new account
+# Create new account
 sudo dscl . -create /Users/thawhakyi
 sudo dscl . -create /Users/thawhakyi UserShell /bin/bash
-sudo dscl . -create /Users/thawhakyi RealName $4
+sudo dscl . -create /Users/thawhakyi RealName "$4"
 sudo dscl . -create /Users/thawhakyi UniqueID 1001
 sudo dscl . -create /Users/thawhakyi PrimaryGroupID 80
 sudo dscl . -create /Users/thawhakyi NFSHomeDirectory /Users/koolisw
-sudo dscl . -passwd /Users/thawhakyi $1
-sudo dscl . -passwd /Users/thawhakyi $1
+sudo dscl . -passwd /Users/thawhakyi "$1"
 sudo createhomedir -c -u thawhakyi > /dev/null
-sudo dscl . -append /Groups/admin GroupMembership username
+sudo dscl . -append /Groups/admin GroupMembership thawhakyi
 
-#Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+# Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
+  -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
+  -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+# VNC password
+echo "$2" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' \
+ | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+# Restart VNC service
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install ngrok
+# install ngrok
 brew install --cask ngrok
 
-#configure ngrok and start it
-ngrok authtoken $3
-ngrok tcp 5900 &
+# configure ngrok and start tunnel
+ngrok config add-authtoken "$3"
+nohup ngrok tcp 5900 --log=stdout > ngrok.log &
